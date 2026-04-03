69e98dee4fe41b8369a4fce708daaa36e227f147
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-03 19:14:02 -0300

    Docs fix

--- src/main/java/edu/itba/exchange/interfaces/Fetch.java
+++ src/main/java/edu/itba/exchange/interfaces/Fetch.java
@@ -34,19 +34,59 @@ public interface Fetch {
      */
     Response post(final URL target, final Options options);
 
+    /**
+     * Provides an options object
+     *
+     * @apiNote I don't know how to feel about this
+     * @return The options 'builder' (?)
+     */
     Options getOptions();
 
+    /**
+     * HTTP options object
+     */
     interface Options {
+        /**
+         * Append an HTTP header
+         *
+         * @apiNote If the headers exists, it overwrites them
+         * @param key   The header key
+         * @param value The header value
+         * @return this, for chaining
+         */
         Options addHeader(final String key, final String value);
 
+        /**
+         * Get the headers
+         *
+         * @return Map of headers key to value
+         */
         Map<String, String> getHeaders();
     }
 
+    /**
+     * HTTP response object
+     */
     interface Response {
+        /**
+         * The response text
+         *
+         * @return HTTP response body
+         */
         String body();
 
+        /**
+         * The response status number
+         *
+         * @return HTTP response status
+         */
         int status();
 
+        /**
+         * Checks the return status
+         *
+         * @return True if the status code is 2XX
+         */
         boolean ok();
     }
 }
