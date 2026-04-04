40683ac89559f0526872f1ab58db20f229897f26
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-04-04 15:05:48 -0300

    added sealed interface

---
+++ src/main/java/edu/itba/exchange/AvailableCurrenciesResult.java
@@ -0,0 +1,10 @@
+package edu.itba.exchange;
+
+import java.util.Currency;
+import java.util.List;
+
+public sealed interface AvailableCurrenciesResult permits AvailableCurrenciesResult.Success, AvailableCurrenciesResult.Failure {
+    record Success(List<Currency> currencies) implements AvailableCurrenciesResult {}
+
+    record Failure(String errorMessage) implements AvailableCurrenciesResult {}
+}
