4afe13ac4db88a4c112c2d076ed71ccf8e0a8ac4
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-04 04:03:24 -0300

    Add more Rate constructors

--- src/main/java/edu/itba/exchange/models/Rate.java
+++ src/main/java/edu/itba/exchange/models/Rate.java
@@ -5,11 +5,18 @@ import java.util.Currency;
 import java.time.LocalDate;
 
 public record Rate(Currency from, Currency to, BigDecimal value, LocalDate rateDate) {
-    public Rate(Currency from, Currency to, String value, LocalDate rateDate) {
-        this(from, to, new BigDecimal(value), rateDate);
+    public Rate(Currency from, Currency to, BigDecimal value) {
+        this(from, to, value, LocalDate.now());
     }
 
     public Rate(Currency from, Currency to, String value) {
-        this(from, to, new BigDecimal(value), LocalDate.now());
+        this(from, to, new BigDecimal(value));
     }
+
+    public Rate(Currency from, String to, BigDecimal value) {
+        this(from, Currency.getInstance(to), value);
+    }
+
+    // At this Rate (pun intended), we should add the 2⁴ combinations
+    // of String to Currency/BigDecimal + LocalDate :)
 }
