c0d0847b7f3301775f60c1d546a2b18667171b54
micaelaperillo <micaelaperillo26@gmail.com>
2026-04-12 17:39:03 -0300

    clean some stuff

--- src/main/java/edu/itba/exchange/services/ApplicationPropertiesProvider.java
+++ src/main/java/edu/itba/exchange/services/ApplicationPropertiesProvider.java
@@ -10,9 +10,13 @@ import edu.itba.exchange.interfaces.PropertiesProvider;
 public record ApplicationPropertiesProvider(Properties properties) implements PropertiesProvider {
     private static final String FILE_PATH = "application.properties";
 
-    public ApplicationPropertiesProvider{try(
-    final var in = new FileInputStream(FILE_PATH)){properties.load(in);}catch(
-    IOException e){throw new RuntimeException(FILE_PATH+" not found!");}}
+    public ApplicationPropertiesProvider {
+        try (final var in = new FileInputStream(FILE_PATH)) {
+            properties.load(in);
+        } catch(IOException e) {
+            throw new RuntimeException(FILE_PATH+" not found!");
+        }
+    }
 
     @Override
     public String get(final String name) {
