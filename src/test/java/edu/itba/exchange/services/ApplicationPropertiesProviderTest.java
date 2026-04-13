6e560a3429040f7d2860c736bf37815047c02cb9
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-12 23:11:38 -0300

    Improve application properties tests

--- src/test/java/edu/itba/exchange/services/ApplicationPropertiesProviderTest.java
+++ src/test/java/edu/itba/exchange/services/ApplicationPropertiesProviderTest.java
@@ -1,6 +1,8 @@
 package edu.itba.exchange.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -9,55 +11,23 @@ import java.util.Properties;
 import org.junit.jupiter.api.*;
 
 class ApplicationPropertiesProviderTest {
-    private static final String FILE_PATH = "application.properties";
-    private static final String BAK_PATH = "application.properties.bak";
-    
-    private static void createFile(String content) throws IOException {
-        try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            writer.write(content);
-        }
-    }
-
-    @BeforeEach
-    void setUp() throws IOException {
-        File file = new File(FILE_PATH);
-        if (file.exists()) {
-            file.renameTo(new File(BAK_PATH));
-        }
-        createFile("PROP1=VAL1\nPROP2=VAL2");
-    }
-
-    @AfterEach
-    void tearDown() {
-        File file = new File(FILE_PATH);
-        if (file.exists()) {
-            file.delete();
-        }
-        File bak = new File(BAK_PATH);
-        if (bak.exists()) {
-            bak.renameTo(new File(FILE_PATH));
-        }
-    }
-
     @Test
     void shouldLoadPropertiesSuccessfully() {
-        ApplicationPropertiesProvider provider = new ApplicationPropertiesProvider(new Properties());
-        assertEquals("VAL1", provider.get("PROP1"));
-        assertEquals("VAL2", provider.get("PROP2"));
+        final var provider = new ApplicationPropertiesProvider();
+
+        assertThat(provider.get("PROP1"), is("VAL1"));
+        assertThat(provider.get("PROP2"), is("VAL2"));
     }
 
     @Test
     void shouldThrowExceptionWhenPropertyNotFound() {
-        ApplicationPropertiesProvider provider = new ApplicationPropertiesProvider(new Properties());
+        final var provider = new ApplicationPropertiesProvider();
+
         assertThrows(NullPointerException.class, () -> provider.get("NON_EXISTENT_PROPERTY"));
     }
 
     @Test
     void shouldThrowExceptionWhenFileNotFound() {
-        File file = new File(FILE_PATH);
-        if (file.exists()) {
-            file.delete();
-        }
-        assertThrows(RuntimeException.class, () -> new ApplicationPropertiesProvider(new Properties()));
+        assertThrows(IllegalStateException.class, () -> new ApplicationPropertiesProvider("fake.properties"));
     }
 }
