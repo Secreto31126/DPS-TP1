b4d28a6b2fd613599208da90c7d28aff3d56fa92
NCasella <nicolascasella02@gmail.com>
2026-04-14 13:19:03 -0300

    100% ApiError coverage

--- src/main/java/edu/itba/exchange/exceptions/ApiError.java
+++ src/main/java/edu/itba/exchange/exceptions/ApiError.java
@@ -2,24 +2,14 @@ package edu.itba.exchange.exceptions;
 
 public record ApiError(ApiErrorCategory category, String message) {
     private final static int CLIENT_ERROR_CLASS = 4;
-    private final static int SERVER_ERROR_CLASS = 5;
 
     public static ApiError networkError(final String message) {
         return new ApiError(ApiErrorCategory.NETWORK_ERROR, message);
     }
-
     public static ApiError fromHttpStatus(final int status, final String message) {
         int statusClass = status / 100;
-        final ApiErrorCategory category = switch (statusClass) {
-            case CLIENT_ERROR_CLASS -> ApiErrorCategory.CLIENT_ERROR;
-            case SERVER_ERROR_CLASS -> ApiErrorCategory.SERVER_ERROR;
-            default -> ApiErrorCategory.UNKNOWN_ERROR;
-        };
+        final ApiErrorCategory category = statusClass == CLIENT_ERROR_CLASS ? ApiErrorCategory.CLIENT_ERROR : ApiErrorCategory.SERVER_ERROR;
         final String formattedMsg = String.format("(Status code %d) %s", status, message);
         return new ApiError(category, formattedMsg);
-    }
-
-    public static ApiError invalidResponseError() {
-        return new ApiError(ApiErrorCategory.INVALID_RESPONSE_ERROR, "The API payload is broken. Not our fault :]");
     }
 }
--- src/main/java/edu/itba/exchange/services/ApplicationPropertiesProvider.java
+++ src/main/java/edu/itba/exchange/services/ApplicationPropertiesProvider.java
@@ -1,21 +1,22 @@
 package edu.itba.exchange.services;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
 
 import edu.itba.exchange.interfaces.PropertiesProvider;
 
-public record ApplicationPropertiesProvider(Properties properties, String path) implements PropertiesProvider {
+public class ApplicationPropertiesProvider implements PropertiesProvider {
     private static final String DEFAULT_FILE_PATH = "application.properties";
+    private final Properties properties;
 
-    public ApplicationPropertiesProvider {
+    public ApplicationPropertiesProvider(Properties properties, String path) {
         try (final var in = getClass().getClassLoader().getResourceAsStream(path)) {
             properties.load(in);
         } catch (final IOException | NullPointerException e) {
             throw new IllegalStateException(path + " not found!");
         }
+        this.properties=properties;
     }
 
     public ApplicationPropertiesProvider(final String path) {
